d6bc572ddb937cdd81a02e5313aa30dcff7e050c
ArturYeg <yeggart1988@gmail.com>
2021-08-05 00:12:03 +0400

    first commit

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+function Weather (){
+    return (
+        <div>
+              <input type="text" placeholder="Inpute city" className = "Input"></input>
+              <button type="button" className="Button">Search</button>
+        </div>
+    )
+}
+
+export default Weather;
